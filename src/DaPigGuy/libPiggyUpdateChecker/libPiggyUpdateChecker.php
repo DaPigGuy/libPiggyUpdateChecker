1aa4ccf6d5b0b97488db61a265b613be8aca4432
aericio <16523741+Aericio@users.noreply.github.com>
2022-02-21 17:23:24 -1000

    Remove hasInitiated check

--- src/DaPigGuy/libPiggyUpdateChecker/libPiggyUpdateChecker.php
+++ src/DaPigGuy/libPiggyUpdateChecker/libPiggyUpdateChecker.php
@@ -10,19 +10,14 @@ use pocketmine\Server;
 
 class libPiggyUpdateChecker
 {
-    public static bool $hasInitiated = false;
-
     public static function init(string $name): void
     {
-        if (!self::$hasInitiated) {
-            self::$hasInitiated = true;
-
-            $plugin = self::getPlugin($name);
-            if ($plugin) Server::getInstance()->getAsyncPool()->submitTask(new CheckUpdatesTask($name, $plugin->getDescription()->getVersion()));
-        }
+        $plugin = self::getPlugin($name);
+        if ($plugin) Server::getInstance()->getAsyncPool()->submitTask(new CheckUpdatesTask($name, $plugin->getDescription()->getVersion()));
     }
 
-    public static function getPlugin(String $name): ?Plugin {
+    public static function getPlugin(string $name): ?Plugin
+    {
         $plugin = Server::getInstance()->getPluginManager()->getPlugin($name);
         if (!$plugin) {
             Server::getInstance()->getLogger()->error("Plugin " . $name . " not found.");
